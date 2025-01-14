eca75730cf6642131292e761529c34743cdf682d
aefafifah <aefgoescollege@gmail.com>
2025-01-14 13:55:10 +0700

    push env

---
+++ database/migrations/2025_01_14_064608_create_supervisor_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supervisor', function (Blueprint $table) {
+            $table->id();
+            $table->string('sv_name');
+            $table->string('sv_email')->unique();
+            $table->string('sv_photo')->nullable();
+            $table->string('pin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supervisor');
+    }
+};
