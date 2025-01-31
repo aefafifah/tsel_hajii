a9a3984f35904242fd79c1b6ee01adf6251540f3
bilionr <158534858+bilionr@users.noreply.github.com>
2025-01-31 09:04:34 +0700

    Update left-drawer.blade.php

--- resources/views/components/supvis/left-drawer.blade.php
+++ resources/views/components/supvis/left-drawer.blade.php
@@ -10,11 +10,11 @@
             </li>
 
             <x-nav-link href="/supvis/home" :active="request()->is('supvis/home')">Home</x-nav-link>
-            <x-nav-link href="/produk" :active="request()->is('produk')">Produk</x-nav-link>
-            <x-nav-link href="/merch" :active="request()->is('merch')">Merch</x-nav-link>
+            <x-nav-link href="/produk" :active="request()->is('produk/*', 'produk')">Produk</x-nav-link>
+            <x-nav-link href="/merch" :active="request()->is('merch', 'merch/*')">Merch</x-nav-link>
             <x-nav-link href="/tambah-sales" :active="request()->is('tambah-sales')">Add Sales</x-nav-link>
             <x-nav-link href="/sales-checklist" :active="request()->is('sales-checklist')">Checklist Sales </x-nav-link>
-            <x-nav-link href="/supvis/riwayat-transaksi" :active="request()->is('/supvis/RiwayatTransaksi')">Riwayat Transaksi</x-nav-link>
+            <x-nav-link href="/supvis/riwayat-transaksi" :active="request()->is('supvis/riwayat-transaksi')">Riwayat Transaksi</x-nav-link>
             @if(Auth::user() && Auth::user()->is_superuser)
                 <x-nav-link href="/tambah-supvis" :active="request()->is('tambah-supvis')">Add Supervisor</x-nav-link>
             @endif
